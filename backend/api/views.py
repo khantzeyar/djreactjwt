20149247579cb9d8d223a11b7cdfccf464401ffb
khantzeyar <khla0002@student.monash.edu>
2023-11-17 12:17:46 +0800

    Implemented the register view in the backend.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    #Check if the username exists
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username is already taken'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #Create a new user
+    user = User.objects.create_user(username=username, password=password)
+
+    #Managing JWT Tokens
+    refresh_token = RefreshToken.for_user(user)
+    access_token = str(refresh_token.access_token)
+
+    return Response({'access_token': access_token}, status=status.HTTP_201_CREATED)
